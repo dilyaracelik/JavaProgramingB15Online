b205fcc3ec8b4e2a1674ae6c8cc3c3746b279d88
sevimzini <sevim.zini@gmail.com>
2020-03-13 00:13:33 -0400

    "Find Largest of 3 Numbers"

---
+++ src/javaInterviewQuestions/FindLargestOf3Numbers.java
@@ -0,0 +1,23 @@
+package javaInterviewQuestions;
+
+public class FindLargestOf3Numbers {
+    public static void main(String[] args) {
+
+        int num1 = 30;
+        int num2 = 12;
+        int num3 = 34;
+
+
+        if (num1 > num2 && num1 > num3) {
+            System.out.println(num1 + " is largest number.");
+        } else if (num2 > num1 && num2 > num3) {
+
+            System.out.println(num2 + " is largest number.");
+        } else {
+
+            System.out.println(num3 + " is largest number.");
+        }
+
+
+    }
+}
