7132c72117e2177c4520cb281ee630e363275f8a
sevimzini <sevim.zini@gmail.com>
2020-05-07 18:50:38 -0400

    array bubble sort

---
+++ src/ArraysFurkanMentoring/BubbleSortArray.java
@@ -0,0 +1,26 @@
+package ArraysFurkanMentoring;
+
+import java.util.Arrays;
+
+public class BubbleSortArray {
+    public static void main(String[] args) {
+
+        int[] arr = {6, 5, 3, 1, 8, 7, 2, 4, 0, -1, -4};
+
+
+        System.out.println("Arrays.toString(arr) = " + Arrays.toString(arr));
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length - 1 - i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+
+        System.out.println("Arrays.toString(arr) = " + Arrays.toString(arr));
+
+    }
+}
